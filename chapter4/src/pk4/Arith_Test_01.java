c0933b86b52e3b2466d05187e1c52c65f244cd92
이준영 <qlqhtlrrl45@naver.com>
2021-06-24 16:59:58 +0900

    as

---
+++ chapter4/src/pk4/Arith_Test_01.java
@@ -0,0 +1,18 @@
+package pk4;
+
+public class Arith_Test_01 {
+
+	public static void main(String[] args) {
+		
+		int a = 12;
+		int b = 5;
+		
+		System.out.println("더하기 : " +(a+b));
+		System.out.println("빼기 : " + (a-b));
+		System.out.println("곱하기 : " +(a*b));
+		System.out.println("나누기 : " +(a/b));
+		System.out.println("나머지 : " +(a%b));
+		
+	}
+
+}
